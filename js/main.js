fcfcc16072845829b985c1afe2c16bb296fa9dd7
Michelle <vand0432@algonquinlive.com>
2016-10-04 10:23:26 -0400

    Making template for portfolio pieces

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+var $f1t = $('.f-1-t');
+var $f1m = $('.f-1-m');
 
+var $f2t = $('.f-2-t');
+var $f2m = $('.f-2-m');
+
+var $f3t = $('.f-3-t');
+var $f3m = $('.f-3-m');
+
+var $f4t = $('.f-4-t');
+var $f4m = $('.f-4-m');
+
+var $f5t = $('.f-5-t');
+var $f5m = $('.f-5-m');
+
+var $f6t = $('.f-6-t');
+var $f6m = $('.f-6-m');
+
+
+
+$f1m.on('mouseover', function () {
+	$f1t.addClass('show');
+});
+
+$f1m.on('mouseout', function () {
+	$f1t.removeClass('show');
+})
+
+$f2m.on('mouseover', function () {
+	$f2t.addClass('show');
+});
+
+$f2m.on('mouseout', function () {
+	$f2t.removeClass('show');
+})
